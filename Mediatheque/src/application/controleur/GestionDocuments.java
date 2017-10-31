770bbde826bcf3efab7169287b434c08b4247100
je.lapointe <je.lapointe@790-442-19.interne.college-gerald-godin.qc.ca>
2017-10-31 18:11:11 -0400

    halloween patch

---
+++ Mediatheque/src/application/controleur/GestionDocuments.java
@@ -0,0 +1,18 @@
+package application.controleur;
+
+import application.modele.Document;
+
+public class GestionDocuments {
+	public void ajouterDocument(){
+		
+	}
+	public void supprimerDocuments(){
+		
+	}
+	public Document RechercherDocument(String strMotRecherche,boolean booRechercheMotClée){
+			
+		return null;
+	}
+	
+	
+}
---
+++ Mediatheque/src/application/controleur/GestionPrets.java
@@ -0,0 +1,9 @@
+package application.controleur;
+
+public class GestionPrets {
+	public void emprunterDocument(){
+		//valider dispo document 
+		//valider si personne peut emprunter
+		
+	}
+}
