82a5efbabadeb7fc60d612041fb8b81a1290c6b4
bloc-notes <Philippedoyon@outlook.com>
2017-10-31 21:39:49 -0400

    le reste du dernier

--- Mediatheque/src/application/modele/Periodique.java
+++ Mediatheque/src/application/modele/Periodique.java
@@ -7,7 +7,7 @@ public class Periodique extends Document {
 	private int intNoVolume;
 	private int intNoPeriodique;
 	
-	public Periodique(String strCodeDocument, String strTitre, Date dateParution, Etat etatDoc) {
+	public Periodique(String strCodeDocument, String strTitre, Date dateParution, Etat etatDoc, int intNoVolume, int intNoPeriodique) {
 		super(strCodeDocument, strTitre, dateParution, etatDoc);
 		this.intNoVolume=intNoVolume;
 		this.intNoPeriodique=intNoPeriodique;
