e16aa7733e0cbe652ec5c8c1a789b5ae8b25590a
bloc-notes <Philippedoyon@outlook.com>
2017-10-31 23:25:45 -0400

    gjhyghvghvb

---
+++ Mediatheque/src/application/modele/test.java
@@ -0,0 +1,20 @@
+package application.modele;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class test {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListeDocuments obj = new ListeDocuments();
+		
+		//obj.lstDocument.forEach(System.out :: println);
+	}
+
+}
