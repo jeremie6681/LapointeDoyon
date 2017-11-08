fd99ad2d6019ff3fc769c1d31c8c415687064ac7
bloc-notes <Philippedoyon@outlook.com>
2017-11-07 20:43:00 -0500

    bjhkghvjkgjhkfv

--- Mediatheque/src/application/Main.java
+++ Mediatheque/src/application/Main.java
@@ -1,22 +1,10 @@
 package application;
-	
-import com.sun.media.sound.AlawCodec;
 
 import application.modele.ListeDocuments;
-import application.modele.TypeDocument;
-import application.vue.InterfaceAjouterDocument;
-import application.vue.InterfaceLoginAdherent;
-import application.vue.InterfaceLoginPrepose;
-import application.vue.InterfaceNouvelUtilisateur;
+import application.modele.TypePersonne;
 import application.vue.InterfacePrincipale;
-import application.vue.InterfaceTypeConnection;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.ButtonType;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
@@ -28,8 +16,8 @@ public class Main extends Application {
 			//InterfaceLoginAdherent ifLogin = new InterfaceLoginAdherent();
 			//InterfaceNouvelUtilisateur ifLogin= new InterfaceNouvelUtilisateur();
 			//InterfaceAjouterDocument ifLogin = new InterfaceAjouterDocument();
-			//InterfacePrincipale ifLogin = new InterfacePrincipale();
-			InterfaceAjouterDocument ifLogin = new InterfaceAjouterDocument();
+			InterfacePrincipale ifLogin = new InterfacePrincipale(primaryStage,TypePersonne.Prepose);
+			//InterfaceAjouterDocument ifLogin = new InterfaceAjouterDocument();
 			primaryStage.setScene(ifLogin.getScene());
 			
 			primaryStage.setResizable(false);
