4655ce512db85b39be9b8c27c1072d1dad22382c
bloc-notes <Philippedoyon@outlook.com>
2017-11-23 16:58:31 -0500

    Nettyage classe liste

--- Mediatheque/src/application/modele/ListeDocuments.java
+++ Mediatheque/src/application/modele/ListeDocuments.java
@@ -16,13 +16,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import application.controleur.GestionDocuments;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
-import javafx.collections.ListChangeListener.Change;
 
+/**
+ * @Description Récupère les données des documents en mémoire par lecture ou
+ *              sérialisation et désérialisation. Les données sont aussi
+ *              entreposé dans la classe.
+ * 
+ * @Implements Serializable
+ * 
+ * @author Lapointe & Doyon
+ *
+ */
 public final class ListeDocuments implements Serializable {
 	/**
 	 * 
@@ -32,161 +39,139 @@ public final class ListeDocuments implements Serializable {
 	private final String strNomFichierLivre = "Donnee/Livres.txt";
 	private final String strNomFichierPeriodique = "Donnee/Periodiques.txt";
 	private final String strNomFichierSerialiser = "Donnee/lstDocuments.ser";
-	
-	
-	
+
+	//Liste contenant les documents séparé par type
 	private ObservableList<Document> lstLivre = FXCollections.observableArrayList();
 	private ObservableList<Document> lstDvd = FXCollections.observableArrayList();
 	private ObservableList<Document> lstPeriodique = FXCollections.observableArrayList();
-	
+
 	public Map<TypeDocument, ObservableList<Document>> mapDocument = new HashMap<>();
-	
-	
-	private static ListeDocuments instanceDoc ;      
-	
+
+	private static ListeDocuments instanceDoc;
+
 	private ListeDocuments() {
 		mapDocument.put(TypeDocument.Livre, lstLivre);
 		mapDocument.put(TypeDocument.Periodique, lstPeriodique);
 		mapDocument.put(TypeDocument.Dvd, lstDvd);
-		
-		//si pas de fichier sérialiser touver
-		
+
 		File fichierSerialiser = new File(strNomFichierSerialiser);
-		
-		//S'il y a eu déjà une sérialisation
-		if(fichierSerialiser.exists()) {
+
+		// S'il y a eu déjà une sérialisation
+		if (fichierSerialiser.exists()) {
 			deserialisation();
-		}
-		else {
+		} else {
 			lectureFichierOriginal();
 		}
-		
-		
-		mapDocument.get(TypeDocument.Livre).forEach(System.out :: println);
-
 	}
-	
+
 	private void lectureFichierOriginal() {
-		//Lecture fichier (2 = dvd, 0 = livre, 1 = periodique)
-		for(int intTypeDocument = 0; intTypeDocument < 3;intTypeDocument++) {
+		// Lecture fichier (2 = dvd, 0 = livre, 1 = periodique)
+		for (int intTypeDocument = 0; intTypeDocument < 3; intTypeDocument++) {
 			BufferedReader brFichier = null;
 			try {
-				if(intTypeDocument == 2) {
+				if (intTypeDocument == 2) {
 					brFichier = new BufferedReader(new FileReader(strNomFichierDVD));
-				}
-				else if (intTypeDocument == 0) {
+				} else if (intTypeDocument == 0) {
 					brFichier = new BufferedReader(new FileReader(strNomFichierLivre));
-				}
-				else {
+				} else {
 					brFichier = new BufferedReader(new FileReader(strNomFichierPeriodique));
 				}
-				
+
 			} catch (FileNotFoundException e) {
 				// TODO: handle exception
 				e.printStackTrace();
 			}
-			
+
 			String strLigne;
-			
+
 			try {
 				while ((strLigne = brFichier.readLine()) != null) {
 					String[] tabLigne = strLigne.split(",");
-					
-					LocalDate dateDocument = LocalDate.parse(tabLigne[2].trim(), DateTimeFormatter.ofPattern("dd-MM-uuuu"));
-					
-					//Crée le document et l'ajoute dans la liste selon son type
-					if(intTypeDocument == 2) {
-						DVD objDVD = new DVD(tabLigne[0].trim(), tabLigne[1].trim(), dateDocument , Etat.DISPONIBLE, Short.parseShort(tabLigne[3].trim()), tabLigne[4].trim());
+
+					LocalDate dateDocument = LocalDate.parse(tabLigne[2].trim(),
+							DateTimeFormatter.ofPattern("dd-MM-uuuu"));
+
+					// Crée le document et l'ajoute dans la liste selon son type
+					if (intTypeDocument == 2) {
+						DVD objDVD = new DVD(tabLigne[0].trim(), tabLigne[1].trim(), dateDocument, Etat.DISPONIBLE,
+								Short.parseShort(tabLigne[3].trim()), tabLigne[4].trim());
 						lstDvd.add(objDVD);
-					}
-					else if (intTypeDocument == 0) {
-						Livre objLivre = new Livre(tabLigne[0].trim(), tabLigne[1].trim(), dateDocument, Etat.DISPONIBLE, tabLigne[3].trim());
+					} else if (intTypeDocument == 0) {
+						Livre objLivre = new Livre(tabLigne[0].trim(), tabLigne[1].trim(), dateDocument,
+								Etat.DISPONIBLE, tabLigne[3].trim());
 						lstLivre.add(objLivre);
-					}
-					else {
-						Periodique objPeriodique = new Periodique(tabLigne[0].trim(), tabLigne[1].trim(), dateDocument, Etat.DISPONIBLE, Integer.parseInt(tabLigne[3].trim()), Integer.parseInt(tabLigne[4].trim()));
+					} else {
+						Periodique objPeriodique = new Periodique(tabLigne[0].trim(), tabLigne[1].trim(), dateDocument,
+								Etat.DISPONIBLE, Integer.parseInt(tabLigne[3].trim()),
+								Integer.parseInt(tabLigne[4].trim()));
 						lstPeriodique.add(objPeriodique);
-					}	
+					}
 				}
-				
-				//Parcours la liste de documents pour ajouter des mots clé
-				mapDocument.get(TypeDocument.values()[intTypeDocument]).forEach(f ->GestionDocuments.motCleAjout(f));
+
+				// Parcours la liste de documents pour ajouter des mots clé
+				mapDocument.get(TypeDocument.values()[intTypeDocument]).forEach(f -> GestionDocuments.motCleAjout(f));
 			} catch (Exception e) {
 				// TODO: handle exception
 				e.printStackTrace();
 				System.out.println(e);
 			}
-	
 		}
-		
-		
 	}
-	
+
 	public void serialisation() {
-		try { 
-		    
-			FileOutputStream fichier = new FileOutputStream(strNomFichierSerialiser); 
-			ObjectOutputStream oos = new ObjectOutputStream(fichier);    
-			
-			Map<TypeDocument, List<Document>> maptempo = new HashMap<TypeDocument,List<Document>>();
-			
-			//Passage d'observableList à des arrayList
+		try {
+			FileOutputStream fichier = new FileOutputStream(strNomFichierSerialiser);
+			ObjectOutputStream oos = new ObjectOutputStream(fichier);
+
+			Map<TypeDocument, List<Document>> maptempo = new HashMap<TypeDocument, List<Document>>();
+
+			// Passage d'observableList à des arrayList
 			maptempo.put(TypeDocument.Livre, new ArrayList<>(mapDocument.get(TypeDocument.Livre)));
 			maptempo.put(TypeDocument.Periodique, new ArrayList<>(mapDocument.get(TypeDocument.Periodique)));
 			maptempo.put(TypeDocument.Dvd, new ArrayList<>(mapDocument.get(TypeDocument.Dvd)));
-			
+
 			oos.writeObject(maptempo);
 			oos.close();
 			fichier.close();
-			System.out.println("sérialisation des documents terminée avec succès");
-			}catch (IOException e) {      
-			e.printStackTrace();    
-			} 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	private void deserialisation() {
-		 
+
 		try {
-		 FileInputStream fichier = new FileInputStream(strNomFichierSerialiser); 
-		 ObjectInputStream ois = new ObjectInputStream(fichier);
-		 
-		 Map<TypeDocument, List<Document>> mapTempo = (HashMap<TypeDocument, List<Document>>) ois.readObject();
-		 
-		 //Passage d'arrayList à des onservableList
-		 mapDocument.get(TypeDocument.Livre).addAll(mapTempo.get(TypeDocument.Livre));
-		 this.lstLivre = mapDocument.get(TypeDocument.Livre);
-		 
-		 mapDocument.get(TypeDocument.Periodique).addAll(mapTempo.get(TypeDocument.Periodique));
-		 this.lstPeriodique = mapDocument.get(TypeDocument.Periodique);
-		 
-		 mapDocument.get(TypeDocument.Dvd).addAll(mapTempo.get(TypeDocument.Dvd));
-		 this.lstDvd = mapDocument.get(TypeDocument.Dvd);
-		 
-		 ois.close();
-		 fichier.close();
-		 System.out.println("désérialisation des documents terminée avec succès");
-		 
-		 
-		 ///
-		 ///reche
-		 ///
-		 
-		 
-		 
-		 
-		 
-		}catch(IOException e) {
+			FileInputStream fichier = new FileInputStream(strNomFichierSerialiser);
+			ObjectInputStream ois = new ObjectInputStream(fichier);
+
+			Map<TypeDocument, List<Document>> mapTempo = (HashMap<TypeDocument, List<Document>>) ois.readObject();
+
+			// Passage d'arrayList à des onservableList
+			mapDocument.get(TypeDocument.Livre).addAll(mapTempo.get(TypeDocument.Livre));
+			this.lstLivre = mapDocument.get(TypeDocument.Livre);
+
+			mapDocument.get(TypeDocument.Periodique).addAll(mapTempo.get(TypeDocument.Periodique));
+			this.lstPeriodique = mapDocument.get(TypeDocument.Periodique);
+
+			mapDocument.get(TypeDocument.Dvd).addAll(mapTempo.get(TypeDocument.Dvd));
+			this.lstDvd = mapDocument.get(TypeDocument.Dvd);
+
+			ois.close();
+			fichier.close();
+		} catch (IOException e) {
 			System.err.println("erreur avec la lecture des ficher de sérialisation des documents");
 			e.printStackTrace();
-		}catch(ClassNotFoundException e2) {
+		} catch (ClassNotFoundException e2) {
 			System.err.println("erreur avec la désérialisation des documents");
 		}
 	}
 	
+	//S'assure que la classe n'est qu'une seul et unique instance
 	public static ListeDocuments getInstance() {
-		  if (instanceDoc == null) {
-			  instanceDoc = new ListeDocuments();
-		  }
-		  return instanceDoc;
+		if (instanceDoc == null) {
+			instanceDoc = new ListeDocuments();
+		}
+		return instanceDoc;
 	}
 }
--- Mediatheque/src/application/modele/ListePersonnes.java
+++ Mediatheque/src/application/modele/ListePersonnes.java
@@ -1,139 +1,144 @@
 package application.modele;
 
 import java.io.BufferedReader;
-import java.io.Externalizable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectInput;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-public class ListePersonnes implements Serializable{
-	
+/**
+ * @Description Récupère les données des utilisateurs en mémoire par lecture ou
+ *              sérialisation et désérialisation. Les données sont aussi
+ *              entreposé dans la classe.
+ * 
+ * @Implements Serializable
+ * 
+ * @author Lapointe & Doyon
+ *
+ */
+
+public class ListePersonnes implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2361481750244889545L;
 	private final String strNomFichierPersonne = "Donnee/Personnes.txt";
 	private final String strNomFichierSerialiser = "Donnee/lstPersonnes.ser";
-	
+
 	private ObservableList<Personne> lstPrepose = FXCollections.observableArrayList();
 	private ObservableList<Personne> lstAdherent = FXCollections.observableArrayList();
 	private ObservableList<Personne> lstAdmin = FXCollections.observableArrayList();
-	
+
 	public Map<TypePersonne, ObservableList<Personne>> mapPersonne = new HashMap<>();
-	
-	
+
 	private static ListePersonnes instanceLst;
-	
+
 	public ListePersonnes() {
 		mapPersonne.put(TypePersonne.Adherent, lstAdherent);
 		mapPersonne.put(TypePersonne.Prepose, lstPrepose);
 		mapPersonne.put(TypePersonne.Admin, lstAdmin);
-		
-		//si pas de fichier sérialiser touver
-		
+
+		// si pas de fichier sérialiser touver
+
 		File fichierSerialiser = new File(strNomFichierSerialiser);
-		
-		//S'il y a eu déjà une sérialisation
-		if(fichierSerialiser.exists()) {
+
+		// S'il y a eu déjà une sérialisation
+		if (fichierSerialiser.exists()) {
 			deserialisation();
-		}
-		else {
+		} else {
 			lecture();
 		}
-
 	}
-	
+
 	public void serialisation() {
-		try { 
-		    
-			FileOutputStream fichier = new FileOutputStream(strNomFichierSerialiser); 
-			ObjectOutputStream oos = new ObjectOutputStream(fichier);    
-			
-			//Passage d'un observableList à des arrayLists
+		try {
+			FileOutputStream fichier = new FileOutputStream(strNomFichierSerialiser);
+			ObjectOutputStream oos = new ObjectOutputStream(fichier);
+
+			// Passage d'un observableList à des arrayLists
 			Map<TypePersonne, List<Personne>> mapTempo = new HashMap<TypePersonne, List<Personne>>();
 			mapTempo.put(TypePersonne.Adherent, new ArrayList<>(mapPersonne.get(TypePersonne.Adherent)));
-			mapTempo.put(TypePersonne.Prepose,new ArrayList<>(mapPersonne.get(TypePersonne.Prepose)));
+			mapTempo.put(TypePersonne.Prepose, new ArrayList<>(mapPersonne.get(TypePersonne.Prepose)));
 			mapTempo.put(TypePersonne.Admin, new ArrayList<>(mapPersonne.get(TypePersonne.Admin)));
-			
+
 			oos.writeObject(mapTempo);
 			oos.close();
 			fichier.close();
-			System.out.println("sérialisation des personnes terminée avec succès");
-			}catch (IOException e) {      
-			e.printStackTrace();    
-			} 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	private void deserialisation() {
 		try {
-			 FileInputStream fichier = new FileInputStream(strNomFichierSerialiser); 
-			 ObjectInputStream ois = new ObjectInputStream(fichier);
-			 
-			 Map<TypePersonne, List<Personne>> mapTempo = (HashMap<TypePersonne, List<Personne>>) ois.readObject();
-			 
-			 //Passage d'arraylist à des observableList
-			 mapPersonne.get(TypePersonne.Adherent).addAll(mapTempo.get(TypePersonne.Adherent));
-			 this.lstAdherent = mapPersonne.get(TypePersonne.Adherent);
-			 
-			 mapPersonne.get(TypePersonne.Prepose).addAll(mapTempo.get(TypePersonne.Prepose));
-			 this.lstPrepose = mapPersonne.get(TypePersonne.Prepose);
-			 
-			 mapPersonne.get(TypePersonne.Admin).addAll(mapTempo.get(TypePersonne.Admin));
-			 this.lstAdmin = mapPersonne.get(TypePersonne.Admin);
-			 ois.close();
-			 fichier.close();
-			 System.out.println("désérialisation des personnes terminée avec succès");
-			 
-			}catch(IOException e) {
-				System.err.println("erreur avec la lecture des ficher de sérialisation des personnes");
-				e.printStackTrace();
-			}catch(ClassNotFoundException e2) {
-				System.err.println("erreur avec la désérialisation des Personnes");
-			}
+			FileInputStream fichier = new FileInputStream(strNomFichierSerialiser);
+			ObjectInputStream ois = new ObjectInputStream(fichier);
+
+			Map<TypePersonne, List<Personne>> mapTempo = (HashMap<TypePersonne, List<Personne>>) ois.readObject();
+
+			// Passage d'arraylist à des observableList
+			mapPersonne.get(TypePersonne.Adherent).addAll(mapTempo.get(TypePersonne.Adherent));
+			this.lstAdherent = mapPersonne.get(TypePersonne.Adherent);
+
+			mapPersonne.get(TypePersonne.Prepose).addAll(mapTempo.get(TypePersonne.Prepose));
+			this.lstPrepose = mapPersonne.get(TypePersonne.Prepose);
+
+			mapPersonne.get(TypePersonne.Admin).addAll(mapTempo.get(TypePersonne.Admin));
+			this.lstAdmin = mapPersonne.get(TypePersonne.Admin);
+
+			ois.close();
+			fichier.close();
+
+		} catch (IOException e) {
+			System.err.println("erreur avec la lecture des ficher de sérialisation des personnes");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e2) {
+			System.err.println("erreur avec la désérialisation des Personnes");
 		}
-	
+	}
+
 	private void lecture() {
 		BufferedReader brFichier = null;
-		
+
 		try {
 			brFichier = new BufferedReader(new FileReader(strNomFichierPersonne));
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
+
 		String strLigne;
 		try {
-			while((strLigne = brFichier.readLine()) != null) {
+			while ((strLigne = brFichier.readLine()) != null) {
 				String[] tabLigne = strLigne.split(",");
-				
-				//Selon le type de personne, il crée l'object et le range dans la liste approprié
+
+				// Selon le type de personne, il crée l'object et le range dans la liste
+				// approprié
 				switch (tabLigne[0].substring(0, 3)) {
 				case "Adm":
-					Prepose objAdministrateur = new Prepose(tabLigne[1], tabLigne[2], tabLigne[3], tabLigne[4], tabLigne[5], tabLigne[0]);
+					Prepose objAdministrateur = new Prepose(tabLigne[1], tabLigne[2], tabLigne[3], tabLigne[4],
+							tabLigne[5], tabLigne[0]);
 					lstAdmin.add(objAdministrateur);
 					break;
 				case "Pre":
-					Prepose objPreposer = new Prepose(tabLigne[1], tabLigne[2], tabLigne[3], tabLigne[4], tabLigne[5], tabLigne[0]);
+					Prepose objPreposer = new Prepose(tabLigne[1], tabLigne[2], tabLigne[3], tabLigne[4], tabLigne[5],
+							tabLigne[0]);
 					lstPrepose.add(objPreposer);
 					break;
 				case "Adh":
-					Adherent objAdherent = new Adherent(tabLigne[1], tabLigne[2], tabLigne[3], tabLigne[4], tabLigne[0]);
-					lstAdherent.add(objAdherent);;
+					Adherent objAdherent = new Adherent(tabLigne[1], tabLigne[2], tabLigne[3], tabLigne[4],
+							tabLigne[0]);
+					lstAdherent.add(objAdherent);
+					;
 					break;
 
 				default:
@@ -144,21 +149,21 @@ public class ListePersonnes implements Serializable{
 			// TODO: handle exception
 		}
 	}
+	
+	//Met à jour les prêts et surtout les amendes
 	public void miseAjourPrets() {
 		for (Personne personne : lstAdherent) {
-			for(Pret pret : personne.getLstPrets()) {
+			for (Pret pret : personne.getLstPrets()) {
 				pret.gestionAmende();
 			}
 		}
 	}
 	
-	
+	//S'assure que la classe est une seul instance
 	public static ListePersonnes getInstance() {
-					//////////////////////////////////////////peut-etre updater les amendes(et les noix) lorque l'on passe ici  
-		  if (instanceLst == null) {
-			  instanceLst = new ListePersonnes();
-		  }
-		  return instanceLst;
+		if (instanceLst == null) {
+			instanceLst = new ListePersonnes();
+		}
+		return instanceLst;
 	}
-
 }
